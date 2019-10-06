d753edd86fd82d351e42dc6667da6f9d0af44d39
george <mugohead@outlook.com>
2019-10-06 15:06:24 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by 郭毅 on 2019/10/6.
+//  Copyright © 2019 郭毅. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
